8a9368a62784a6689af91457d7144dc351677914
aprameyo8858 <146671193+aprameyo8858@users.noreply.github.com>
2025-01-11 19:44:18 +0530

    Update run_gwgan_cnn.sh

--- run_gwgan_cnn.sh
+++ run_gwgan_cnn.sh
@@ -5,7 +5,7 @@ export MPLBACKEND="agg"
 # flags:
 ## add flag --cuda to run script on GPUs, otherwise do not add flag
 
-#python3 main_gwgan_cnn.py --data fmnist --num_epochs 100 --beta 35
-#python3 main_gwgan_cnn.py --data cifar_gray --num_epochs 100 --beta 40
+#python3 main_gwgan_cnn.py --data fmnist --num_epochs 100 --beta 35  --cuda
+#python3 main_gwgan_cnn.py --data cifar_gray --num_epochs 100 --beta 40  --cuda
 python3 main_gwgan_cnn.py --data mnist --num_epochs 100 --beta 32 --cuda
 # (beta for MNIST: 32, for gray-scale CIFAR: 40)
